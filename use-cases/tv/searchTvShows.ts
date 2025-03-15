94f835f0c920d408db4dac8dc8bcf751c296285b
Sheldon Nofer <snofer@bethel.jw.org>
2025-03-14 22:22:03 -0400

    Deploy

--- use-cases/tv/searchTvShows.ts
+++ use-cases/tv/searchTvShows.ts
@@ -8,8 +8,8 @@ import { load } from "https://deno.land/std@0.186.0/dotenv/mod.ts";
  * @returns {Promise<Result<SearchResult<TvShow>>>}
  */
 export async function searchTvShows(searchTerm: string, page = 1) {
-  const env = Deno.env.toObject();
-  let TMDB_API_KEY = env.TMDB_API_KEY;
+  const env = await load();
+  const TMDB_API_KEY = env.TMDB_API_KEY ?? Deno.env.get("TMDB_API_KEY");
   const options = {
     method: "GET",
     headers: {
